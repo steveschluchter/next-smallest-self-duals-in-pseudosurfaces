ccb660b313ee92b9e9c26cba2d4d1c27baeade30
jmlazaro <juan.lazaro2599@gmail.com>
2023-12-30 22:55:39 -0800

    intro Graph class

--- j.py
+++ j.py
@@ -2,52 +2,106 @@ import os
 from glob import glob
 import networkx as nx
 from argparse import ArgumentParser
+from sys import exit as sys_exit
 
 from typing import Optional
 
-TEXT_DIR = 'perm_txts'
-EDGE_TO_ID_MAP = dict[tuple[int], int]
-ID_TO_EDGE_MAP = dict[int, tuple[int]]
 
-def perm_from_txt(perm_path: str) -> tuple[int]:
+TEXT_DIR = 'perm_txts'
+PERM = tuple[int]
+NODE = int
+EDGE = tuple[NODE]
+EDGE_TO_ID_MAP = dict[EDGE, NODE]
+ID_TO_EDGE_MAP = dict[NODE, EDGE]
+
+
+def get_perm(perm_path: str) -> PERM:
     with open(file_path) as f:
         number_line = f.read()
     number_line = number_line[len('The permutation ('):].split(')')[0]
     number_line = tuple(int(num) for num in number_line.split(', '))
     return number_line
 
-def graph_from_txt(perm_path: str) -> nx.classes.graph.Graph:
-    gf = os.path.splitext(os.path.split(perm_path)[-1])[0].split('-')[0]
-    return nx.read_edgelist(gf, nodetype=int)
 
-def vstar(graph: nx.classes.graph.Graph, v) -> tuple[tuple[int]]:
-    return tuple((min(edge), max(edge)) for edge in graph.edges(v))
+class Graph():
+    def __init__(file_path: str, file_type: str) -> None:
+        if file_type == 'graph':
+            self.graph = nx.read_edgelist(file_path, nodetype=int)
+        elif file_type == 'perm':
+            self.graph = self.graph_from_perm(file_path)
+        else:
+            sys_exit("Bad file_type. Use 'graph' or 'perm'")
 
-def dual_edges(
-        vstar: tuple[tuple[int]],
-        perm: tuple[int],
-        edge_ids: EDGE_TO_ID_MAP,
-        id_edges: ID_TO_EDGE_MAP,
-    ) -> tuple[tuple[int]]:
-    return tuple((id_edges[perm[edge_ids[edge] - 1]] for edge in vstar))
+        self.degree_six_nodes = get_degree_sixes()
+        self.edge_id_map = get_edge_id_map()
+        self.id_edge_map = get_id_edge_map()
 
-def boundry_walk():
-    ...
+    def graph_from_perm(perm_path: str) -> nx.classes.graph.Graph:
+        gf = os.path.splitext(os.path.split(perm_path)[-1])[0].split('-')[0]
+        return nx.read_edgelist(gf, nodetype=int)
 
-def get_n_umbrealls(boundry_walks: ...) -> tuple[int]:
-    ...
+    def get_degree_sixes(self) -> tuple[NODE]:
+        return tuple(
+            node for node in self.graph.nodes if self.graph.degree(node) == 6
+        )
+
+    def get_edge_id_map(self) -> EDGE_TO_ID_MAP:
+        return {edge: id for id, edge in enumerate(self.graph.edges)}
+
+    def get_id_edge_map(self) -> ID_TO_EDGE_MAP:
+        return {id: edge for id, edge in enumerate(self.graph.edges)}
+
+    def get_vstar(self, node: NODE) -> tuple[EDGE]:
+        return tuple((min(edge), max(edge)) for edge in self.graph.edges(node))
+
+    def get_dual_edges(
+            self,
+            vstar: tuple[EDGE],
+            perm: PERM
+        ) -> tuple[EDGE]:
+        return tuple(
+            self.id_edge_map[perm[self.edge_id_map[edge] - 1]]
+            for edge in vstar
+        )
+
+    def get_dual_face(self, node: NODE, perm: PERM) -> nx.classes.graph.Graph
+        return nx.Graph(
+            self.get_dual_edges(self.get_vstar(self.graph, node), perm)
+        )
+
+    def check_self_dual_perm(perm: PERM): -> bool:
+        for node in self.graph.nodes:
+            if not nx.is_eulerian(self.get_dual_face(node, perm)):
+                return False
+        return True
+
+    def check_components(self, perm: PERM) -> bool:
+        def inverse_perm(self, perm: PERM) -> PERM:
+            return tuple()
+
+
+    def check_cycle(self, node: NODE, perm: PERM) -> bool:
+        dual_face = self.get_dual_face(node, perm)
+        node_degrees = dict(dual_face.degree(dual_face.nodes))
+        for degree in node_degrees.values():
+            if degree != 2:
+                return False
+        return True
+
+    def check_bowtie(self, node: NODE, perm: PERM) -> bool:
+        ...
 
 def process_perm(
         perm_path: str,
-        graph: Optional[nx.classes.graph.Graph] = None,
-        edge_ids: Optional[EDGE_TO_ID_MAP] = None,
-        id_edges: Optional[ID_TO_EDGE_MAP] = None
+        graph: Graph,
     ) -> None:
-    perm = perm_from_txt(perm_path)
-    if not graph:
-        graph = graph_from_txt(perm_path)
-        edge_ids = {edge: id for id, edge in enumerate(graph.edges)}
-        id_edges = {id: edge for id, edge in enumerate(graph.edges)}
+    perm = get_perm(perm_path)
+
+    if not check_self_dual(graph, perm, edge_id_map, id_edge_map):
+        return
+
+    if not degree_sixes:
+        return
 
     boundry_walks = get_boundry_walks(...)
     n_umbrellas = get_n_umbrellas(boundry_walks)
@@ -64,17 +118,12 @@ def main() -> None:
         os.path.join(TEXT_DIR, perm_pattern)
     ))[:args.max_perms]
 
-    if args.graph_file:
-        graph = nx.read_edgelist(args.graph_file, nodetype=int)
-        edge_ids = {edge: id for id, edge in enumerate(graph.edges)}
-        id_edges = {id: edge for id, edge in enumerate(graph.edges)}
-    else:
-        graph = None
-        edge_ids = None
-        id_edges = None
+    graph = Graph(args.graph_file, 'graph') if args.graph_file else None
 
     for perm_file in perm_files:
-        process_perm(perm_file, graph, edge_ids, id_edges)
+        if not graph:
+            graph = Graph(perm_file, 'perm')
+        process_perm(perm_file, graph)
 
 if __name__ == '__main__':
     main()
