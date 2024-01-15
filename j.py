7586c6eab00121ec9c7ad4dc2282b9befd65d899
jmlazaro <juan.lazaro2599@gmail.com>
2024-01-14 17:33:44 -0800

    documentation; doc strings, remove used methods; check all 14 edges used in edge_count

--- j.py
+++ j.py
@@ -20,6 +20,7 @@ RESULTS_DIR = 'results'
 PERM = tuple[int]
 NODE = int
 EDGE = tuple[NODE]
+PASS = tuple[NODE]
 EDGE_TO_ID_MAP = dict[EDGE, NODE]
 ID_TO_EDGE_MAP = dict[NODE, EDGE]
 DIV_LENGTH = 60
@@ -33,6 +34,7 @@ def log_info(
     res_file: Optional[TextIOWrapper] = None,
     out: Optional[str] = 'all'
 ) -> None:
+    """ Log to screena and/or results file """
     ALL_STR = 'all'
     if out in (STANDARD_OUT_STR, ALL_STR):
         print(mssg)
@@ -41,13 +43,19 @@ def log_info(
     return
 
 def get_perm(perm_path: str) -> PERM:
+    """ Parse individual perm from ADC-logging script """
     with open(perm_path) as f:
         number_line = f.read()
     number_line = number_line[len('The permutation ('):].split(')')[0]
     number_line = tuple(int(num) for num in number_line.split(', '))
     return number_line
 
-def rotation_scheme(seq: list[tuple[NODE]]) -> list[tuple[NODE]]:
+def rotation_scheme(seq: list[PASS]) -> list[tuple[NODE]]:
+    """
+    Gets list of cycles from sequence of edges
+    Example:
+    [(1,2), (2,3), (3,1), (4,5), (5,6), (6,4)] -> [(1,2,3), (4,5,6)]
+    """
     rotations = []
     start_tup = seq.pop(0)
     current = [*start_tup]
@@ -68,21 +76,19 @@ def rotation_scheme(seq: list[tuple[NODE]]) -> list[tuple[NODE]]:
     return rotations
 
 class Graph():
+    """ Extends nx.Graph """
     def __init__(
         self,
         file_path: str,
         res_file: Optional[TextIOWrapper] = None
     ) -> None:
+        """ Store frequently used values """
         self.graph = nx.read_edgelist(file_path, nodetype=int)
         self.degree_six_nodes = self.get_degree_six_nodes()
         self.edge_id_map = self.get_edge_id_map()
         self.id_edge_map = self.get_id_edge_map()
 
         self.res_file = res_file
-
-    def graph_from_perm(perm_path: str) -> nx.classes.graph.Graph:
-        gf = os.path.splitext(os.path.split(perm_path)[-1])[0].split('-')[0]
-        return nx.read_edgelist(gf, nodetype=int)
 
     def get_degree_six_nodes(self) -> tuple[NODE]:
         return tuple(
@@ -96,28 +102,30 @@ class Graph():
         return {id: edge for id, edge in enumerate(self.graph.edges, start=1)}
 
     def get_vstar(self, node: NODE) -> tuple[EDGE]:
+        """
+        Get all edges including node as (vertex_id_1, vertex_id_2)
+        where vertex_id_1 < vertex_id_2
+        """
         return tuple((min(edge), max(edge)) for edge in self.graph.edges(node))
 
     def get_dual_edges(self, vstar: tuple[EDGE], perm: PERM) -> tuple[EDGE]:
+        """ Get perm (dual) of vstar """
         return tuple(
             self.id_edge_map[perm[self.edge_id_map[edge] - 1]]
             for edge in vstar
         )
 
     def get_dual_face(self, node: NODE, perm: PERM) -> nx.classes.graph.Graph:
+        """ Make nx.Graph object from perm(vstar(node)) """
         return nx.Graph(self.get_dual_edges(self.get_vstar(node), perm))
 
     def check_self_dual_perm(self, perm: PERM) -> bool:
+        """
+        Check that dual_face(vstar(node)) for all nodes in self.graph have a
+        closed walk that includes each edge of the graph exactly once.
+        """
         for node in self.graph.nodes:
             if not nx.is_eulerian(self.get_dual_face(node, perm)):
-                return False
-        return True
-
-    def check_cycle(self, node: NODE, perm: PERM) -> bool:
-        dual_face = self.get_dual_face(node, perm)
-        node_degrees = dict(dual_face.degree(dual_face.nodes))
-        for degree in node_degrees.values():
-            if degree != 2:
                 return False
         return True
 
@@ -125,7 +133,14 @@ class Graph():
         self,
         source_node: NODE,
         perm: PERM
-    ) -> list[list[EDGE]]:
+    ) -> list[list[PASS]]:
+        """
+        Get both uses of the four edges including the center of a bowtie that
+        produce valid eulerian walks of the bowtie. Note that the tuples here,
+        (node_1, node_2), represent a pass through two edges
+        (node_1, cross_node) and (cross_node, node_2) where cross_node is the
+        node with four edges in the bowtie = dual(vstar(source_node)).
+        """
         without_cross = self.get_dual_face(source_node, perm)
         for node in without_cross.nodes:
             if without_cross.degree(node) == 4:
@@ -156,11 +171,21 @@ class Graph():
         self,
         degree_six_node: NODE,
         perm: PERM
-    ) -> list[list[EDGE]]:
+    ) -> list[list[PASS]]:
+        """
+        Get list of lists of passes through degree_six_node from
+        dual(vstar(node)) for all nodes in self.groph.
+        Pass (node_1, node2)
+            = (node_1, degree_six_node), (degree_six_node, node_2)
+        """
         pass_seqs = [[]]
         for node in self.graph.nodes:
             dual_face = self.get_dual_face(node, perm)
             if degree_six_node in dual_face.nodes:
+                """
+                If degree_six_node is the center of the bowtie, make a pass_seq
+                for each of the options from get_dual_bowtie_edge_options
+                """
                 if len(tuple(dual_face.neighbors(degree_six_node))) == 4:
                     options = self.get_dual_bowtie_edge_options(node, perm)
                     if pass_seqs == [[]]:
@@ -169,6 +194,8 @@ class Graph():
                         pass_seqs.extend(deepcopy(pass_seqs))
                         for option_ind, option in enumerate(options):
                             pass_seqs[option_ind].extend(option)
+                # If degree_six_node isn't at the center of a bowtie, we know
+                # it only has two neighbors which respresent a pass
                 else:
                     for pass_seq in pass_seqs:
                         pass_seq.append(
@@ -178,6 +205,10 @@ class Graph():
             f'Sequences found for node {degree_six_node}: {len(pass_seqs)}',
             self.res_file
         )
+        """
+        Note interesting casses where two nodes map to bowties with the same
+        center node
+        """
         if len(pass_seqs) > 2:
             log_info(
                 f'Found n_pass_seqs > 2 for node {degree_six_node}',
@@ -190,6 +221,10 @@ class Graph():
         degree_six_node: NODE,
         perm: PERM
     ) -> bool:
+        """
+        Perform rotation_scheme on each pass sequence for degree_six_node. If
+        any produce more than 1 cycle (umbrella), we have found a pinchpoint.
+        """
         seqs = self.get_pass_sequences(degree_six_node, perm)
         for passes in seqs:
             log_info(f'Using pass sequence: {passes}', self.res_file)
@@ -211,6 +246,10 @@ class Graph():
         )
 
     def get_dual_bowtie_nodes(self, perm: PERM) -> dict[NODE, NODE]:
+        """
+        Get dictionary of degree six node(s) (keys), if the dual of their
+        vstar is a bowtie, and the center node of that bowtie (values)
+        """
         dual_bowtie_nodes = {}
         for degree_six_node in self.degree_six_nodes:
             dual_face = self.get_dual_face(degree_six_node, perm)
@@ -220,6 +259,15 @@ class Graph():
         return dual_bowtie_nodes
 
     def check_orientable(self, perm: PERM) -> bool:
+        """
+        Check if it is possible to impose a consistent orientation on
+        dual(vstar(node)) for all nodes in self.graph. This means is it
+        possible to use each edge exacty as many times in one direction
+        (node_1, node_2) as the other (node_2, node_1) over all of their
+        boundry walks.
+        """
+        # Determine how any bowties there are and therefore how many options we
+        # have for the use of the bowtie edges
         dual_bowtie_nodes = self.get_dual_bowtie_nodes(perm)
         n_dual_bowtie_nodes = len(dual_bowtie_nodes)
         if n_dual_bowtie_nodes == 0:
@@ -232,13 +280,21 @@ class Graph():
             self.get_dual_face(node, perm) for node in self.graph.nodes
         ]
 
-        def get_option_instruction(node, option) -> int:
+        def get_option_instruction(
+            node: NODE,
+            option: Union[int, tuple[int]]
+        ) -> int:
+            """
+            Return index to pass to self.get_dual_bowtie_edge_options to
+            instruct which set of passes to use
+            """
             return (
                 option if n_dual_bowtie_nodes == 1
                 else option[list(dual_bowtie_nodes).index(node)]
             )
 
         def get_bowtie_walk(center_node, bowtie_passes) -> list[EDGE]:
+            """ Unpack bowtie passes into walk edges """
             return [
                 (bowtie_passes[0][0], center_node),
                 (center_node, bowtie_passes[0][1]),
@@ -248,11 +304,16 @@ class Graph():
                 (bowtie_passes[1][1], bowtie_passes[0][0])
             ]
 
-        def get_cycle_walk(edges) -> list[EDGE]:
+        def get_cycle_walk(edges: list[EDGE]) -> list[EDGE]:
+            """ Organize cycle edges with rotation_scheme """
             rs = list(rotation_scheme(edges)[0])
             return [tup for tup in zip(rs, rs[1:] + [rs[0]])]
 
         def get_oriented_walk(edge_count, walk) -> tuple[EDGE]:
+            """
+            If any edge in walk present in same direction in edge_count,
+            reverse walk, else return unmodified walk.
+            """
             for edge in walk:
                 if edge in edge_count:
                     walk = tuple(tuple(reversed(edge)) for edge in walk)
@@ -263,6 +324,7 @@ class Graph():
             edge_count = defaultdict(lambda: 0)
             for node, dual_face in zip(self.graph.nodes, dual_faces):
                 if node in dual_bowtie_nodes:
+                    # Get relevant bowtie walk for current node and option
                     bowtie_passes = self.get_dual_bowtie_edge_options(
                         node,
                         perm
@@ -277,13 +339,18 @@ class Graph():
                     for edge in walk:
                         edge_count[edge] = 1
                 else:
+                    # If not the first walk, need to orient walk based on
+                    # previous use of edges
                     walk = get_oriented_walk(edge_count, walk)
                     for n1, n2 in walk:
                         if (n2, n1) in edge_count:
                             edge_count[(n2, n1)] -= 1
                         else:
                             edge_count[(n1, n2)] += 1
-            if all(count == 0 for count in edge_count.values()):
+            if (
+                    all(count == 0 for count in edge_count.values())
+                    and len(edge_count) == 14
+                ):
                 return True
         return False
 
@@ -292,10 +359,15 @@ def process_perm(
         graph: Graph,
         log_level: int
     ) -> str:
+    """
+    Return the 2-complex name, if any, in which graph.graph may be embedded
+    where perm is assumed to represent an algebraic dual correspondence (ADC).
+    """
     if isinstance(perm, str):
         perm = get_perm(perm)
 
     if not graph.check_self_dual_perm(perm):
+        # Log separately if not an ADC to avoid excessive logging
         if log_level > 1:
             log_info('', graph.res_file)
             log_info('-' * DIV_LENGTH, graph.res_file)
@@ -333,7 +405,10 @@ def process_perm(
 def main() -> None:
     parser = ArgumentParser()
     parser.add_argument('graph_file')
-    parser.add_argument('perm_pattern')
+    parser.add_argument(
+        'perm_pattern',
+        help='Use `all` to evaluate all perms instead of using perm files'
+    )
     parser.add_argument('-m', dest='max_perms', type=int, default=None)
     parser.add_argument('-r', dest='refresh_results', action='store_true')
     parser.add_argument('-v', dest='log_level', type=int, default=1)
